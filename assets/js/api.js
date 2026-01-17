53a2d8052b53098b8998a6a960ff34d3ff0fae91
sdcomayagua <sdcomayagua@gmail.com>
2026-01-16 23:54:36 -0600

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,9 @@
+// === API Google Apps Script ===
+const API_URL = "https://script.google.com/macros/s/AKfycbytPfD9mq__VO7I2lnpBsqdCIT119ZT0zVyz0eeVjrJVgN_q8FYGgmqY6G66C2m67Pa4g/exec";
+
+async function apiGetAll() {
+  const res = await fetch(API_URL, { cache: "no-store" });
+  if (!res.ok) throw new Error("Error al conectar con la API");
+  return await res.json();
+}
 
