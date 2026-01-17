c29620df96a887736cb3caa8cbf3bb9237bed501
sdcomayagua <sdcomayagua@gmail.com>
2026-01-16 23:55:34 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
+async function init() {
+  const data = await apiGetAll();
+
+  const productos = data.productos || [];
+  const grid = document.getElementById("grid");
+
+  grid.innerHTML = "";
+
+  productos.forEach(p => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <img src="${p.imagen || ""}">
+      <h3>${p.nombre}</h3>
+      <p>Lps. ${p.precio}</p>
+      <button>Ver producto</button>
+    `;
+
+    card.onclick = () => openProduct(p);
+    grid.appendChild(card);
+  });
+}
+
+init();
 
